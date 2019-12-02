a4fcba5d12b583ab7ad41645adec4f535916778a
Ignacio Guerra <ignacio.guerra@nayra.coop>
2019-12-02 08:15:54 -0300

    db

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap'
+
+const characters = [
+  { name: 'Santiago Cafiero', role: 'Jefe de Gabinete', confirmed: true, description: '', age: null },
+  { name: 'Julio Vitobello', role: 'Secretaría General de la Presidencia', confirmed: false, description: '', age: null },
+  { name: 'Vilma Ibarra', role: 'Secretaría Legal y Técnica', confirmed: true, description: '', age: null },
+  { name: 'Matías Kulfas', role: 'Ministerio de Economía', confirmed: true, description: '', age: null },
+  { name: 'Martín Guzmán', role: 'Secretaría de finanzas', confirmed: false, description: '', age: null },
+  { name: 'José Ignacio De Mendiguren', role: 'Secretaría de Producción', confirmed: false, description: '', age: null },
+  { name: 'Miguel Ángel Pesce', role: 'Titular del Banco Central', confirmed: true, description: '', age: null },
+  { name: 'Felipe Solá', role: 'Ministerio de Relaciones Exteriores', confirmed: true, description: '', age: null },
+  { name: 'Eduardo "Wado" de Pedro', role: 'Ministerio del Interior', confirmed: true, description: '', age: null },
+  { name: 'Daniel Arroyo', role: 'Ministerio de Desarrollo Social', confirmed: true, description: '', age: null },
+  { name: 'Sergio Lanziani', role: 'Ministerio de Energía', confirmed: false, description: '', age: null },
+  { name: 'Alberto Hensel', role: 'Ministerio de Minería', confirmed: false, description: '', age: null },
+  { name: 'Marcela Losardo', role: 'Ministerio de Justicia', confirmed: true, description: '', age: null },
+  { name: 'Ginés González García', role: 'Ministerio de Salud', confirmed: false, description: '', age: null },
+  { name: 'María Eugenia Bielsa', role: 'Ministerio de Hábitat y Vivienda', confirmed: true, description: '', age: null },
+  { name: 'Gabriel Katopodis', role: 'Ministerio de Obras Públicas o Infraestructura', confirmed: true, description: '', age: null },
+  { name: 'Agustín Rossi', role: 'Ministro de Defensa', confirmed: true, description: '', age: null },
+  { name: '???', role: 'Ministerio de Seguridad', confirmed: false, description: '', age: null },
+  { name: 'Claudio Moroni', role: 'Ministerio de Trabajo', confirmed: true, description: '', age: null },
+  { name: 'Victoria Donda', role: 'Ministerio de Igualdad y DD.HH.', confirmed: false, description: '', age: null },
+  { name: 'Carlos Caserio', role: 'Ministerio de Transporte', confirmed: false, description: '', age: null },
+  { name: 'Nicolás Trotta', role: 'Ministerio de Educación', confirmed: false, description: '', age: null },
+  { name: '??? Tristán Bauer o Alejandro Grimson', role: 'Ministerio de Cultura', confirmed: false, description: '', age: null },
+  { name: 'Roberto Salvarezza', role: 'Ministerio de Ciencia y Tecnología', confirmed: false, description: '', age: null },
+  { name: '??? F. Meritello o Leandro Santoro', role: 'Secretaría de Medios Públicos', confirmed: false, description: '', age: null },
+  { name: 'Matías Lammens', role: 'Secretaría de Deportes', confirmed: false, description: '', age: null },
+  { name: 'Marco Lavagna', role: 'Titular del INDEC', confirmed: false, description: '', age: null },
+  { name: 'Carlos Zannini', role: 'Procurador del Tesoro', confirmed: false, description: '', age: null }
+]
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <Container className="App">
+        sdaf sdf
+      </Container>
+    );
+  }
+}
+
+export default App;
